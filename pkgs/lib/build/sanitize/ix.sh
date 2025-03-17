629dd87c2df4c0bca591ba3b2dda9bc0f250b41f
Ivan Komarov <Ivan.Komarov@dfyz.info>
2025-03-17 03:07:01 +0100

    ASan integration WIP (doesn't work yet)

--- pkgs/lib/build/sanitize/ix.sh
+++ pkgs/lib/build/sanitize/ix.sh
@@ -3,6 +3,6 @@
 {% block lib_deps %}
 {% if sanitize %}
 lib/build/sanitize/{{sanitize}}
-lib/compiler_rt/sanitizer(sanitize=)
+lib/compiler_rt/sanitize/{{sanitize}}(sanitize=)
 {% endif %}
 {% endblock %}
