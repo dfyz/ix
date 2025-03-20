834903b135dffa6ab36b74c87af857fe3291e707
Ivan Komarov <Ivan.Komarov@dfyz.info>
2025-03-20 04:15:36 +0100

    WIP

--- pkgs/lib/compiler_rt/sanitize/t/ix.sh
+++ pkgs/lib/compiler_rt/sanitize/t/ix.sh
@@ -15,6 +15,43 @@ sed -i \
 sed -i \
   '/volatile auto x = &kDynamic;/d' \
   compiler-rt/lib/interception/interception.h
+
+# Force using the fast unwinder. The slow unwinder uses `libunwind`,
+# which introduces circular dependencies when building `lib/c++`.
+# We compile sanitized binaries with `-fno-omit-frame-pointer`,
+# so the fast unwinder should work fine.
+sed -i \
+    's/define SANITIZER_CAN_SLOW_UNWIND 1/define SANITIZER_CAN_SLOW_UNWIND 0/' \
+    compiler-rt/lib/sanitizer_common/sanitizer_stacktrace.h
+
+# Create stubs for:
+#   * the `libunwind` functions used by the slow unwinder
+#   * `dlsym()` (TODO: implement a `dlsym()` that can be used for interception)
+sed -i \
+  '/SANITIZER_SOURCES_NOTERMINATION/a sanitizer_fakes.cpp' \
+  compiler-rt/lib/sanitizer_common/CMakeLists.txt
+
+cat << EOF > compiler-rt/lib/sanitizer_common/sanitizer_fakes.cpp
+#include <stdio.h>
+#include <stdlib.h>
+
+#define _GNU_SOURCE 1
+#include <unwind.h>
+
+extern "C" {
+  void* {{uniq_id}}_dlsym(void* handle, const char* symbol) { return NULL; }
+
+  uintptr_t {{uniq_id}}__Unwind_GetIP(struct _Unwind_Context*) {
+    fprintf(stderr, "_Unwind_GetIP() was not supposed to be called\n");
+    abort();
+  }
+
+  _Unwind_Reason_Code {{uniq_id}}__Unwind_Backtrace(_Unwind_Trace_Fn, void *) {
+    fprintf(stderr, "_Unwind_Backtrace() was not supposed to be called\n");
+    abort();
+  }
+}
+EOF
 {% endblock %}
 
 {% block cmake_flags %}
@@ -43,4 +80,10 @@ export LDFLAGS="-resource-dir=${out} \${LDFLAGS}"
 {{super()}}
 mkdir -p ${out}/include
 cp -R compiler-rt/include/sanitizer ${out}/include
+{% endblock %}
+
+{% block c_rename_symbol %}
+_Unwind_GetIP
+_Unwind_Backtrace
+dlsym
 {% endblock %}
