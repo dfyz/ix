e1f84cb530336b04fd63580f36b49e9d1aac1493
Ivan Komarov <Ivan.Komarov@dfyz.info>
2025-03-21 03:57:43 +0100

    better fakes

--- pkgs/lib/compiler_rt/sanitize/t/fakes.cpp
+++ pkgs/lib/compiler_rt/sanitize/t/fakes.cpp
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,8 +18,20 @@ extern "C" {
   }
 
   __attribute__((weak))
-  _Unwind_Reason_Code _Unwind_Backtrace(_Unwind_Trace_Fn, void *) {
+  _Unwind_Reason_Code _Unwind_Backtrace(_Unwind_Trace_Fn, void*) {
     fprintf(stderr, "_Unwind_Backtrace() was not supposed to be called\n");
     abort();
   }
+
+  __attribute__((weak))
+  void* __dynamic_cast(const void*, const void*, const void*, ptrdiff_t) {
+    fprintf(stderr, "__dynamic_cast() was not supposed to be called\n");
+    abort();
+  }
+
+  // Symbols for various fake typeinfos (they are not functions but whatever).
+  __attribute__((weak)) void _ZTIN10__cxxabiv121__vmi_class_type_infoE() {}
+  __attribute__((weak)) void _ZTIN10__cxxabiv120__si_class_type_infoE() {}
+  __attribute__((weak)) void _ZTIN10__cxxabiv117__class_type_infoE() {}
+  __attribute__((weak)) void _ZTISt9type_info() {}
 }
