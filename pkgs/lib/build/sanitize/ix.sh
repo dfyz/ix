fbeed95b57cf67bc0481be3a20de2be2e2610463
Ivan Komarov <Ivan.Komarov@dfyz.info>
2025-05-04 17:38:33 +0200

    better

--- pkgs/lib/build/sanitize/ix.sh
+++ pkgs/lib/build/sanitize/ix.sh
@@ -2,6 +2,9 @@
 
 {% block lib_deps %}
 {% if sanitize %}
+{% if not linux %}
+{{ix.error('Building with sanitizers is currently only supported on Linux')}}
+{% endif %}
 lib/build/sanitize/{{sanitize}}
 lib/compiler_rt/sanitize/{{sanitize}}(sanitize=)
 {% endif %}
