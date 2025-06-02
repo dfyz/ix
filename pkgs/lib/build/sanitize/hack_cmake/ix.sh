5d02d4ff700cf6aa43d07eaedb4d46ba46d930cf
Ivan Komarov <Ivan.Komarov@dfyz.info>
2025-06-02 23:14:44 +0200

    ignore-all has an unfortunate side-effect of messing up symbol presence detection (i.e., we now believe we have __cxa_thread_atexit_impl())

--- pkgs/lib/build/sanitize/hack_cmake/ix.sh
+++ pkgs/lib/build/sanitize/hack_cmake/ix.sh
@@ -5,9 +5,19 @@ When building LLVM components with sanitizer support, the sanitizer runtime cont
 to some of the function defined by the components. Normally, this is not a problem, since we
 will resolve this references down the line when linking the final binary. However, CMake refuses
 to build anything if it can't succesfully link a simple binary at this point, so we have to trick
-it into believing it can by ignoring all unresolved symbol errors.
+it into believing it can by ignoring some unresolved symbol errors.
 #}
 
+{% block undef %}
+_Unwind_Backtrace
+_Unwind_GetIP
+__real___cxa_throw
+__real___cxa_rethrow_primary_exception
+__real__Unwind_RaiseException
+{% endblock %}
+
 {% block env %}
-export LDFLAGS="-Wl,--unresolved-symbols=ignore-all ${LDFLAGS}"
+{% for x in ix.parse_list(self.undef()) %}
+export LDFLAGS="-Wl,--defsym={{x}}=0 ${LDFLAGS}"
+{% endfor %}
 {% endblock %}
