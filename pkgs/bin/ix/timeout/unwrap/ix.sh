ccc3ee1c02420d7cfd2b8e58184fc937f2b5a8ef
Ivan Komarov <Ivan.Komarov@dfyz.info>
2025-12-08 01:00:41 +0100

    hackily port the coreutils fix to m4 (and add a FIXME for coreutils)

--- pkgs/bin/ix/timeout/unwrap/ix.sh
+++ pkgs/bin/ix/timeout/unwrap/ix.sh
@@ -1,6 +1,7 @@
 {% extends '//die/c/autohell.sh' %}
 
 {% block fetch %}
+{# FIXME: upgrade this back to 9.9 after cross-compilation issues are solved #}
 https://ftp.gnu.org/gnu/coreutils/coreutils-9.5.tar.gz
 767ae6a22950ec42f3ba5f7c1de79dd27800ee8e9b8642da5dedb5974a1741e5
 {% endblock %}
