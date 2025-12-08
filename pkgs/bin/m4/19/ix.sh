71fb90aaecf76862365831d75a7d174445ad8a80
Ivan Komarov <Ivan.Komarov@dfyz.info>
2025-12-08 20:51:56 +0100

    revert a redundant m4 fix

--- pkgs/bin/m4/19/ix.sh
+++ pkgs/bin/m4/19/ix.sh
@@ -4,12 +4,3 @@
 {{super()}}
 --with-libsigsegv
 {% endblock %}
-
-{% block configure %}
-{{super()}}
-# pthread rwlock broken on aarch64
-cat << EOF > tests/Makefile
-all:
-install:
-EOF
-{% endblock %}
